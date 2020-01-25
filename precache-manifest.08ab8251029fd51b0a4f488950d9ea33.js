fe031deb400f45e84809a4d3cbbfc89af8c1fab9
CuteShaun <andrewtsapko@gmail.com>
2020-01-26 01:51:23 +0200

    Updates

---
+++ precache-manifest.08ab8251029fd51b0a4f488950d9ea33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20603c18799ba26283b72da16dbee14d",
+    "url": "/table-form/index.html"
+  },
+  {
+    "revision": "b0cffefd729653aa8e00",
+    "url": "/table-form/static/css/main.48271a93.chunk.css"
+  },
+  {
+    "revision": "455a0b2baa44a4b6c838",
+    "url": "/table-form/static/js/2.6423d4cc.chunk.js"
+  },
+  {
+    "revision": "bcfcfd76ffa5e8bd579b4e2a4f75269f",
+    "url": "/table-form/static/js/2.6423d4cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0cffefd729653aa8e00",
+    "url": "/table-form/static/js/main.a3a17115.chunk.js"
+  },
+  {
+    "revision": "86c33cac0ccbade993af",
+    "url": "/table-form/static/js/runtime-main.9320c864.js"
+  }
+]);
